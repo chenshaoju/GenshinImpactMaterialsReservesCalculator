3e3ba4fd3f807ec7f944167044ced548fe978531
Chen Shaoju <chenshaoju@hotmail.com>
2022-02-18 12:46:58 +0800

    添加项目文件。

---
+++ GenshinImpactCraftingReservesCalculator/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BottonCalc = New System.Windows.Forms.Button()
+        Me.TextBoxLevel1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxLevel2 = New System.Windows.Forms.TextBox()
+        Me.TextBoxLevel3 = New System.Windows.Forms.TextBox()
+        Me.TextBox_Name = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBoxLevel4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BottonCalc)
+        Me.GroupBox1.Controls.Add(Me.TextBoxLevel1)
+        Me.GroupBox1.Controls.Add(Me.TextBoxLevel2)
+        Me.GroupBox1.Controls.Add(Me.TextBoxLevel3)
+        Me.GroupBox1.Controls.Add(Me.TextBox_Name)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.TextBoxLevel4)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(309, 73)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "输入/计算"
+        '
+        'BottonCalc
+        '
+        Me.BottonCalc.Location = New System.Drawing.Point(224, 35)
+        Me.BottonCalc.Name = "BottonCalc"
+        Me.BottonCalc.Size = New System.Drawing.Size(75, 23)
+        Me.BottonCalc.TabIndex = 6
+        Me.BottonCalc.Text = "计算并添加"
+        Me.BottonCalc.UseVisualStyleBackColor = True
+        '
+        'TextBoxLevel1
+        '
+        Me.TextBoxLevel1.Location = New System.Drawing.Point(189, 37)
+        Me.TextBoxLevel1.Name = "TextBoxLevel1"
+        Me.TextBoxLevel1.Size = New System.Drawing.Size(29, 21)
+        Me.TextBoxLevel1.TabIndex = 5
+        '
+        'TextBoxLevel2
+        '
+        Me.TextBoxLevel2.Location = New System.Drawing.Point(154, 37)
+        Me.TextBoxLevel2.Name = "TextBoxLevel2"
+        Me.TextBoxLevel2.Size = New System.Drawing.Size(29, 21)
+        Me.TextBoxLevel2.TabIndex = 4
+        '
+        'TextBoxLevel3
+        '
+        Me.TextBoxLevel3.Location = New System.Drawing.Point(119, 37)
+        Me.TextBoxLevel3.Name = "TextBoxLevel3"
+        Me.TextBoxLevel3.Size = New System.Drawing.Size(29, 21)
+        Me.TextBoxLevel3.TabIndex = 3
+        '
+        'TextBox_Name
+        '
+        Me.TextBox_Name.Location = New System.Drawing.Point(9, 37)
+        Me.TextBox_Name.Name = "TextBox_Name"
+        Me.TextBox_Name.Size = New System.Drawing.Size(69, 21)
+        Me.TextBox_Name.TabIndex = 1
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(32, 21)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 12)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "名称"
+        '
+        'TextBoxLevel4
+        '
+        Me.TextBoxLevel4.Location = New System.Drawing.Point(84, 37)
+        Me.TextBoxLevel4.Name = "TextBoxLevel4"
+        Me.TextBoxLevel4.Size = New System.Drawing.Size(29, 21)
+        Me.TextBoxLevel4.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(189, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "一级"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(154, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "二级"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(119, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "三级"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(84, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "四级"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.ListBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 93)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(309, 345)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "数据区"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(224, 316)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "清空数据"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(224, 287)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "删除所选"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(7, 21)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.ScrollAlwaysVisible = True
+        Me.ListBox1.Size = New System.Drawing.Size(211, 316)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(337, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "原神材料储备计算器"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBoxLevel4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxLevel1 As TextBox
+    Friend WithEvents TextBoxLevel2 As TextBox
+    Friend WithEvents TextBoxLevel3 As TextBox
+    Friend WithEvents TextBox_Name As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents BottonCalc As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ GenshinImpactCraftingReservesCalculator/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub BottonCalc_Click(sender As Object, e As EventArgs) Handles BottonCalc.Click
+        Dim CalcResult As String
+        If TextBox_Name.Text = "" Then
+            MsgBox("名称不能为空", MsgBoxStyle.OkOnly + MsgBoxStyle.Exclamation, "错误")
+        Else
+            If TextBoxLevel4.Text = "" Or IsNumeric(TextBoxLevel4.Text) = False Then
+                TextBoxLevel4.Text = 0
+            End If
+            If TextBoxLevel3.Text = "" Or IsNumeric(TextBoxLevel4.Text) = False Then
+                TextBoxLevel3.Text = 0
+            End If
+            If TextBoxLevel2.Text = "" Or IsNumeric(TextBoxLevel4.Text) = False Then
+                TextBoxLevel2.Text = 0
+            End If
+            If TextBoxLevel1.Text = "" Or IsNumeric(TextBoxLevel4.Text) = False Then
+                TextBoxLevel1.Text = 0
+            End If
+            CalcResult = TextBoxLevel4.Text * 4 + TextBoxLevel3.Text * 3 + TextBoxLevel2.Text * 2 + TextBoxLevel1.Text
+            ListBox1.Items.Add(TextBox_Name.Text + "：" + CalcResult)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ListBox1.SelectedItem = "" Then
+            MsgBox("请先选择要删除的条目", MsgBoxStyle.OkOnly + MsgBoxStyle.Exclamation, "错误")
+        Else
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim i As Integer
+        For i = ListBox1.Items.Count - 1 To 0 Step -1
+            ListBox1.Items.RemoveAt(i)
+        Next
+    End Sub
+End Class
